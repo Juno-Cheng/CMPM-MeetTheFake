ae8938bd094b67c741e7f0a440bc8246a1cb70f1
Juno-Cheng <chengjonathan2021@gmail.com>
2023-11-23 00:19:53 -0800

    Added TileMap/TileSet to Game

---
+++ assets/TileSet/customTileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="CustomTileSet" tilewidth="16" tileheight="16" tilecount="100" columns="20">
+ <image source="Desktop/TileMap.png" width="320" height="80"/>
+</tileset>
